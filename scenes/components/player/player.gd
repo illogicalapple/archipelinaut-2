1efb1d87b1b9649ea1c5efa8ebacd7a22b5060b5
UngaBungaPlaying <icheesecake.u@gmail.com>
2024-06-22 17:44:22 -0700

    updated player controller

--- scenes/components/player/player.gd
+++ scenes/components/player/player.gd
@@ -2,13 +2,14 @@ extends CharacterBody2D
 
 @export var speed : float
 @onready var animTree = $AnimationTree
+@onready var player_sprite = $PlayerSprite
 
 var facing_last = 1
-var blend_amount = 0.0
-var target_blend_amount = 1.0
-const BLEND_SPEED = 5.0
+@onready var scale_ref = player_sprite.scale
 
-func _physics_process(delta: float) -> void:
+var waddle_target = 0.0
+
+func _physics_process(_delta: float) -> void:
 	# Main movement 
 	var movement_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = movement_direction * speed
@@ -17,14 +18,15 @@ func _physics_process(delta: float) -> void:
 	# Change direction the character is facing
 	var facing_dir = movement_direction.sign().x
 	if facing_dir != 0 && facing_dir != facing_last:
-		scale.x *= -1
 		facing_last = facing_dir
-
+		var turn_tween = get_tree().create_tween()
+		turn_tween.tween_property(player_sprite, "scale:x", facing_last * scale_ref.x, 0.12)
+	
 	# Play bobbing animation if moving
 	if velocity != Vector2.ZERO:
-		target_blend_amount = 0.0
+		waddle_target = 0.0
 	else:
-		target_blend_amount = 1.0
-		
-	blend_amount = lerp(blend_amount, target_blend_amount, delta * BLEND_SPEED)
-	animTree.set("parameters/walk_idle/blend_amount", blend_amount)
+		waddle_target = 1.0
+	
+	var waddle_tween = get_tree().create_tween()
+	waddle_tween.tween_property(animTree, "parameters/walk_idle/blend_amount", waddle_target, 0.1)
