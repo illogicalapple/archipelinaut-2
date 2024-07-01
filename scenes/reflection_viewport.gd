b0a3380a418ace36e3d547c06cf9d92ba20716b6
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2024-07-01 16:46:54 -0500

    stop drop and merge

--- scenes/reflection_viewport.gd
+++ scenes/reflection_viewport.gd
@@ -6,7 +6,6 @@ func _ready() -> void:
 	var camera = camera_target.get_viewport()
 	world_2d = camera.world_2d
 
-
 func _process(delta: float) -> void:
 	$Camera2D.global_position = camera_target.global_position
 	size = DisplayServer.window_get_size()
