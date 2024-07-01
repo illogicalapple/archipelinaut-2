f5d554e04b7998a7b3545ac8b71faefcbe61d098
SmallConfusion <smallconfusion@protonmail.com>
2024-07-01 18:30:40 -0500

    Tweaked reflection shader, added Reflection2D

---
+++ scripts/nodes/reflection2d.gd
@@ -0,0 +1,18 @@
+extends Node2D
+class_name Reflection2D
+
+@export var reflection_sprite : Sprite2D
+
+var sprite : Sprite2D
+
+func _ready():
+	sprite = Sprite2D.new()
+	sprite.texture = reflection_sprite.texture
+	sprite.z_index = -1
+	add_child(sprite)
+
+
+func _process(_delta):
+	sprite.position = global_position - reflection_sprite.global_position
+	sprite.scale = reflection_sprite.scale * Vector2(1, -1)
+	sprite.rotation = -reflection_sprite.rotation
