936680ee04eb42338e8f4a84dfe3fec8c4803bb3
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2024-06-30 20:49:05 -0500

    patch things

--- scenes/reflection_viewport.gd
+++ scenes/reflection_viewport.gd
@@ -1,12 +1,12 @@
 extends SubViewport
 
+@export var camera_target: Camera2D
 
 func _ready() -> void:
-	var camera = $"../Player/Camera2D".get_viewport()
-	
+	var camera = camera_target.get_viewport()
 	world_2d = camera.world_2d
 
 
 func _process(delta: float) -> void:
-	$Camera2D.global_position = $"../Player/Camera2D".global_position
+	$Camera2D.global_position = camera_target.global_position
 	size = DisplayServer.window_get_size()
