9e727426a9bceee857562cce087fb31a9c4cfa77
SmallConfusion <smallconfusion@protonmail.com>
2024-07-03 18:30:52 -0500

    Added click circle effect

--- scenes/components/terrain/click_indicator.gd
+++ scenes/components/terrain/click_indicator.gd
@@ -3,10 +3,10 @@ extends Sprite2D
 
 func play():
 	var tween = get_tree().create_tween()
-	tween.tween_method(func(t): material.set_shader_parameter("t", t), 0., 1., 1.)
+	tween.tween_method(func(t): material.set_shader_parameter("t", t), 0., 1., .5)
 	
 
 func _input(e):
-	if e is InputEventMouseButton and e.pressed:
+	if e is InputEventMouseButton and e.pressed and e.button_index == 1:
 		global_position = get_global_mouse_position()
 		play()
