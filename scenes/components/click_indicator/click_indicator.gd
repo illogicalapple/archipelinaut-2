149cb8b83d1f076a79ffcd728b5969b2ccc7fcb5
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2024-07-06 10:35:32 -0500

    tweaks

--- scenes/components/click_indicator/click_indicator.gd
+++ scenes/components/click_indicator/click_indicator.gd
@@ -4,9 +4,11 @@ extends Sprite2D
 func play():
 	var tween = get_tree().create_tween()
 	tween.tween_method(func(t): material.set_shader_parameter("t", t), 0., 1., .5)
+	await tween.finished
+	
+	queue_free()
 	
 
-func _input(e):
-	if e is InputEventMouseButton and e.pressed and e.button_index == 1:
-		global_position = get_global_mouse_position()
-		play()
+func _ready():
+	global_position = get_global_mouse_position()
+	play()
---
+++ scenes/components/click_indicator/click_indicator_spawner.gd
@@ -0,0 +1,9 @@
+extends Node2D
+
+var click_indicator = preload("res://scenes/components/click_indicator/click_indicator.tscn")
+
+func _input(event):
+	if event.is_action_pressed("target"):
+		var indicator = click_indicator.instantiate()
+		indicator.global_position
+		add_child(indicator)
