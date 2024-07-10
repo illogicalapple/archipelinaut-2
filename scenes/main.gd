a428778d1f6efab133dbf4f86f7e1f8b08d927e9
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2024-07-09 20:02:58 -0500

    reload chunks (wip)

--- scenes/main.gd
+++ scenes/main.gd
@@ -3,3 +3,8 @@ extends Node2D
 func _process(_delta):
 	%F3Menu.chunks_loaded = $Chunks.get_child_count()
 	%F3Menu.object_count = get_tree().get_nodes_in_group("objects").size()
+
+func _input(event):
+	if event.is_action_pressed("reload_chunks") and Input.is_action_pressed("debug"):
+		for child in $Chunks.get_children():
+			child.queue_free()
