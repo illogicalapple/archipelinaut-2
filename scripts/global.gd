772646676c685a7468c1c2bd562ff806aa1ff2ae
Large-Guy <r.lebgue@gmail.com>
2024-07-11 02:55:14 -0700

    Added ambience

--- scripts/global.gd
+++ scripts/global.gd
@@ -3,6 +3,17 @@ extends Node
 var f3_enabled: bool = false
 var player: CharacterBody2D
 
+var ambience: AudioStream = preload("res://assets/sounds/ocean.wav")
+
+func _ready():
+	await get_tree().process_frame
+	play_ambience()
+
+func play_ambience():
+	print("Playing ambience")
+	await play_sound(ambience,1.0,0)
+	play_ambience()
+
 func play_sound(clip: AudioStream, pitch, volume):
 	var player = AudioStreamPlayer.new()
 	get_tree().root.add_child(player)
