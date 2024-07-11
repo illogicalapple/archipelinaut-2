0e91595a271792431bc01fd8b2890633bd7ca79a
Large-Guy <r.lebgue@gmail.com>
2024-07-11 03:16:37 -0700

    Oops i'm dumb!

---
+++ SoundEffect.gd
@@ -0,0 +1,14 @@
+extends Resource
+class_name SoundEffect
+
+@export var clips: Array[AudioStream]
+
+@export var default_volume: float = 0.0
+@export var volume_variation: float = 0.0
+@export var default_pitch: float = 1.0
+@export var pitch_variation: float = 0.2
+
+func play():
+	var clip = clips.pick_random()
+	if(clip):
+		Global.play_sound(clip,default_pitch + randf_range(-pitch_variation,pitch_variation), default_volume + randf_range(-volume_variation,volume_variation))
---
+++ scripts/sun.gd
@@ -0,0 +1,15 @@
+extends DirectionalLight2D
+
+@export var brightness: Curve
+@export var gradient: Gradient
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var t = Global.time / 24
+	color = gradient.sample(t)
+	energy = brightness.sample(t)
