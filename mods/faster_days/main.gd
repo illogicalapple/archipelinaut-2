759659888c21f0238fa0f2acbc902429dd9971c8
Large-Guy <r.lebgue@gmail.com>
2024-07-14 13:58:51 -0700

    Better modding support

--- mods/faster_days/main.gd
+++ mods/faster_days/main.gd
@@ -1,7 +1,23 @@
 extends Node
 
+var freeze = -1
+var stopped = false
+
 func _ready():
 	print("Mod successfully loaded!")
+	Global.add_command("stop",stop)
+	Global.add_command("start",start)
+
+func stop(args: PackedStringArray):
+	print("Stopping")
+	stopped = true
+	freeze = Global.time
+
+func start(args: PackedStringArray):
+	print("Starting")
+	stopped = false
 
 func _process(delta):
 	Global.time += delta * 10
+	if(stopped):
+		Global.time = freeze
--- scripts/commands.gd
+++ scripts/commands.gd
@@ -4,34 +4,44 @@ var open = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	Global.add_command("quit",quit)
+	Global.add_command("reload",reload)
+	Global.add_command("spawn",spawn)
+	Global.add_command("load_mod",load_mod)
+
+func quit(args: PackedStringArray):
+	get_tree().quit()
+
+func reload(args: PackedStringArray):
+	for child in get_tree().current_scene.get_node("Chunks").get_children():
+		child.queue_free()
+
+func spawn(args: PackedStringArray):
+	var count = (int(args[1]) if args.size() == 2 else 1)
+	if(args[0] == "pig"):
+		for c in range(count):
+			var pig = load("res://scenes/components/objects/mob.tscn")
+			var i = pig.instantiate()
+			i.global_position = Global.player.global_position
+			get_tree().current_scene.add_child(i)
+	if(args[0] == "tree"):
+		for c in range(count):
+			var tree = load("res://scenes/components/objects/tree.tscn")
+			var i = tree.instantiate()
+			i.global_position = Global.player.global_position
+			get_tree().current_scene.add_child(i)
+
+func load_mod(args: PackedStringArray):
+	var access = DirAccess.get_directories_at("mods/")
+	var mod_name = args[0]
+	if(access.has(mod_name)):
+		ModManager.load_mod("mods/"+mod_name+"/")
 
 func interpret(tex: String):
 	var spl: PackedStringArray = tex.split(" ")
-	if(spl[0] == "/quit"):
-		get_tree().quit()
-	elif(spl[0] == "/reload"):
-		for child in get_tree().current_scene.get_node("Chunks").get_children():
-			child.queue_free()
-	elif(spl[0] == "/spawn"):
-		var count = (int(spl[2]) if spl.size() == 3 else 1)
-		if(spl[1] == "pig"):
-			for c in range(count):
-				var pig = load("res://scenes/components/objects/mob.tscn")
-				var i = pig.instantiate()
-				i.global_position = Global.player.global_position
-				get_tree().current_scene.add_child(i)
-		if(spl[1] == "tree"):
-			for c in range(count):
-				var tree = load("res://scenes/components/objects/tree.tscn")
-				var i = tree.instantiate()
-				i.global_position = Global.player.global_position
-				get_tree().current_scene.add_child(i)
-	elif(spl[0] == "/load"):
-		var access = DirAccess.get_directories_at("mods/")
-		var mod_name = spl[1]
-		if(access.has(mod_name)):
-			ModManager.load_mod("mods/"+mod_name+"/")
+	
+	if Global.commands.has(spl[0]):
+		Global.commands[spl[0]].call(spl.slice(1))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
