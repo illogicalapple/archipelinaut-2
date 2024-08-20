f8499aa91f43f4cea1c8abd1b870759da8abc54e
illogicalapple <80916557+illogicalapple@users.noreply.github.com>
2024-08-20 17:21:47 -0500

    inventory and stuff

--- scenes/components/inventory/hotbar.gd
+++ scenes/components/inventory/hotbar.gd
@@ -1,3 +1,10 @@
 extends HBoxContainer
 
-var inventory: Array = ["air", "air", "air", "air", "air"]
+var selected_slot = 0
+var inventory: Array = ["log", "air", "air", "air", "air"]
+var inventory_amounts: Array[int] = [1, 0, 0, 0, 0]
+
+func _input(event: InputEvent) -> void:
+	for slot in range(5):
+		if event.is_action_pressed("slot_" + str(slot)):
+			selected_slot = slot
--- scenes/components/inventory/inventory_slot.gd
+++ scenes/components/inventory/inventory_slot.gd
@@ -5,4 +5,12 @@ extends TextureRect
 @export var watching_index: int = 0
 
 func _process(_delta):
+	if parent.inventory_amounts[watching_index] <= 1:
+		$Amount.hide()
+	if parent.inventory_amounts[watching_index] <= 0:
+		$TextureRect.hide()
+	if parent.selected_slot == watching_index:
+		texture = preload("res://assets/images/ui/slot_selected.png")
+	else:
+		texture = preload("res://assets/images/ui/slot.png")
 	$TextureRect.texture = load("res://assets/images/items/" + parent.inventory[watching_index] + ".png")
